378321b25810ff1acc3debd66aae3ac67cedcfd5
Gianluca Busiello <gianluca.busiello@gmail.com>
2016-10-16 01:30:54 +0100

    test

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if __name__ == '__main__':
         },   
         '/static': {
             'tools.staticdir.on': True,
-            'tools.staticdir.dir': '/public'
+            'tools.staticdir.dir': 'public'
         },
         '/favicon.ico': {
             'tools.staticfile.on': True,
